5c1e0668bb425d4dcefefd02f9490b3e5d86c4b9
Turbiani <leo.turbiani@gmail.com>
2016-07-27 18:01:34 -0300

    Inserindo novo modulo no Gradle, modulo de persistencia

--- app/src/main/java/br/com/turbiani/smartmarvelapp/activity/MainActivity.java
+++ app/src/main/java/br/com/turbiani/smartmarvelapp/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package br.com.turbiani.smartmarvelapp;
+package br.com.turbiani.smartmarvelapp.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import br.com.turbiani.smartmarvelapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
